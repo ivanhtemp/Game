289f8b7acdb361781e244eb4f1d077d96a29da3a
ivanhtemp <ivan.htemprano@gmail.com>
2016-02-09 00:29:46 +0100

    Appearance modificated. JS & CSS added

---
+++ myStyle.js
@@ -0,0 +1,8 @@
+function screen_hunter() {
+
+  document.getElementById("main_rectangle").style.width = screen.availWidth/2;
+  document.getElementById("main_rectangle").style.height = screen.availHeight/2;
+  document.getElementById("main_rectangle").style.left = screen.availWidth/4;
+  document.getElementById("main_rectangle").style.top = screen.availHeight/4;
+
+}
